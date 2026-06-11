7490c7ff1fe2a53f5820e3b6fa0d5fe397496907
Tanuja-Sindkar2006 <tanujasindkar1430@gmail.com>
2026-06-11 19:47:50 +0530

    Add files via upload
    The Simple Portfolio Website is a web-based application designed to showcase personal information, skills, educational background, projects, and contact details. The website provides an easy-to-navigate interface that helps visitors learn more about the individual and their professional achievements. It is developed using HTML, CSS, and JavaScript to ensure a responsive and interactive user experience. This project serves as a digital portfolio and helps establish an online professional presence.

---
+++ portfolio/js/script.js
@@ -0,0 +1,9 @@
+// Dark Mode Toggle
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+// Console check
+console.log("Portfolio Loaded Successfully");
